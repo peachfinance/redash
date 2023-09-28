e9baba5355ec07a96950b3181074be2293262eac
Genki Sugawara <sugawara@winebarrel.jp>
2023-09-28 09:22:00 +0900

    Add Datadog alert destination (#6476)
    * Add Datadog alert destination
    
    * Fix Datadog Event API response code: 200 -> 202
    
    * Add datadog alert dest test
    
    * Sort test_destinations.py imports
    
    * Fix test_datadog_notify_calls_requests_post
    
    * Fix datadog alert dest: Add aggregation_key

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -425,6 +425,7 @@ default_destinations = [
     "redash.destinations.pagerduty",
     "redash.destinations.hangoutschat",
     "redash.destinations.microsoft_teams_webhook",
+    "redash.destinations.datadog",
 ]
 
 enabled_destinations = array_from_string(
